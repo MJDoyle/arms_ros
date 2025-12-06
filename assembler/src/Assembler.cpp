a9c3e55fbd32f575919a8655eafb3a7fa638fefe
MJDoyle <drmatthewjdoyle@gmail.com>
2025-12-06 01:01:50 +0000

    reimplement Prusa and not slic3r

--- assembler/src/Assembler.cpp
+++ assembler/src/Assembler.cpp
@@ -457,8 +457,8 @@ void Assembler::generateSlicerGcode()
 
     std::stringstream command_ss;
 
-    //command_ss << "(cd " << WORKING_DIR << " && " << "prusa-slicer --export-gcode --dont-arrange --merge --output assembler.gcode --load arms_prusa_config.ini";
-    command_ss << "(cd " << WORKING_DIR << " && " << "slic3r --dont-arrange --merge --output assembler.gcode --load arms_prusa_config.ini";
+    command_ss << "(cd " << WORKING_DIR << " && " << "prusa-slicer --export-gcode --dont-arrange --merge --output assembler.gcode --load arms_prusa_config.ini";
+    //command_ss << "(cd " << WORKING_DIR << " && " << "slic3r --dont-arrange --merge --output assembler.gcode --load arms_prusa_config.ini";
 
     for (std::string filename : filenames)
         command_ss << " " << filename;
@@ -758,7 +758,7 @@ void Assembler::generateGrasps()
         //if (part->getType() == Part::INTERNAL)
         //    part->setPPGGrasp(PPGGraspGenerator::generate(part));
 
-        if (part->getType() == Part::EXTERNAL)
-            part->setVacuumGrasp(VacuumGraspGenerator::generate(part));
+        //if (part->getType() == Part::EXTERNAL)
+        //    part->setVacuumGrasp(VacuumGraspGenerator::generate(part));
     }
 }
